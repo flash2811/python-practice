fb20cc8821959fba3795007bb1de06387b7b75c0
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2020-03-31 14:40:26 -0700

    Create helloWord.py

---
+++ helloWord.py
@@ -0,0 +1,58 @@
+print("this is a test")
+# this is a comment
+
+theNumber = 5
+print(5 + theNumber)
+
+theNewNumber = 4 ** 4
+print(theNewNumber)
+
+theVar = print("whoa")
+
+x,y = (3,5)
+print(x)
+print(y)
+
+
+# while loop
+condition = 1
+while condition < 10:
+    print(condition)
+    condition += 1
+
+#while True:
+    #print("Doing stuff")
+
+#for loop
+print("for loop")
+exampleList = [1, 5, 4, 5, 2, 6, 7, 77]
+for eachNumber in exampleList:
+    print(eachNumber)
+
+print("continue program")
+
+for x in range(1,11):
+    print(x)
+
+#if and else statement
+x = 5
+y = 8
+z= 5
+if z < y > x:
+    print("y is greater than z and greater than x")
+if z <= x:
+    print("z is less than or equal to x")
+else:
+    print("greater")
+
+#elif statement
+ex = 5
+ey = 10
+ez = 22
+if ex > ey:
+    print()
+elif ex < ez:
+    print("less")
+else:
+    print("never ran")
+
